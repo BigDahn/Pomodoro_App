a4229b76cf3adf835b7457a3c1eb948afd60e389
BigDahn <harkinborholarholuwah@gmail.com>
2025-05-03 14:31:05 +0100

    first commit

--- src/Modal/SettingsModal.jsx
+++ src/Modal/SettingsModal.jsx
@@ -58,9 +58,9 @@ function SettingsModal() {
 
   return (
     <div
-      className={`bg-white w-[540px] h-[470px] fixed   m-auto z-[99999] rounded-3xl bottom-[100px] font-${initialFont} `}
+      className={`bg-white w-[100%] h-[575px] md:w-[540px] lg:h-[470px] fixed   m-auto z-[99999] rounded-3xl top-[30px] md:bottom-[100px] font-${initialFont} `}
     >
-      <div className="flex items-center justify-between py-6 border-b border-gray-300 ">
+      <div className="flex items-center justify-between md:py-6 border-b border-gray-300 py-3">
         <h3
           className={`text-[#161932] font-bold font-${initialFont} text-4xl px-6`}
         >
@@ -73,22 +73,25 @@ function SettingsModal() {
           onClick={() => dispatch({ type: "closeModal" })}
         />
       </div>
-      <section className="px-6 py-7">
+      <section className="px-6 py-4 md:py-7">
         <div
-          className={`flex flex-col gap-3 border-b pb-5 border-gray-300 font-${initialFont} font-bold text-[#161932]`}
+          className={`flex flex-col  md:items-start gap-3 border-b pb-3 md:pb-5 border-gray-300 font-${initialFont} font-bold text-[#161932]`}
         >
-          <h2>TIME (MINUTES)</h2>
-          <div className="flex gap-5 items-center justify-evenly">
+          <h2 className="m-auto lg:m-0">TIME (MINUTES)</h2>
+          <div className="flex flex-col gap-4 md:flex md:flex-row lg:gap-7 items-center justify-evenly">
             {changeTime.map((s, i) => {
               const { name, defaultTime } = s;
               return (
-                <div className="flex flex-col gap-2" key={i}>
+                <div
+                  className="flex flex-row w-[100%] gap-3 md:flex-col justify-between md:gap-2"
+                  key={i}
+                >
                   <label htmlFor={name} className="text-gray-400">
                     {name}
                   </label>
                   <input
                     type="text"
-                    className="bg-gray-300 h-10 w-[9rem] rounded-md outline-none px-4 relative disabled:cursor-not-allowed"
+                    className="bg-gray-300 h-10 w-[10rem] md:w-[9rem] rounded-md outline-none px-4 relative disabled:cursor-not-allowed"
                     name={name}
                     value={defaultTime}
                     disabled
@@ -157,10 +160,10 @@ function SettingsModal() {
           </div>
         </div>
         <div
-          className={`flex items-center justify-between py-7 border-b border-gray-300 font-${initialFont} font-bold text-[#161932]`}
+          className={`flex flex-col gap-2 md:flex md:flex-row items-center justify-between py-5 md:py-7 border-b border-gray-300 font-${initialFont} font-bold text-[#161932]`}
         >
           <h3>FONTS</h3>
-          <div className="flex justify-evenly items-center gap-2">
+          <div className="flex justify-evenly items-center gap-5 md:gap-2">
             {fonts.map((s, i) => {
               return (
                 <div
@@ -180,10 +183,10 @@ function SettingsModal() {
           </div>
         </div>
         <div
-          className={`flex items-center justify-between py-7 font-bold font-${initialFont} text-[#161932]`}
+          className={`flex flex-col gap-2 md:flex md:flex-row items-center justify-between py-5 md:py-7 font-bold font-${initialFont} text-[#161932]`}
         >
           <h3>COLOR</h3>
-          <div className="flex justify-evenly items-center gap-2">
+          <div className="flex justify-evenly items-center md:gap-2 gap-5">
             {color.map((s, index) => {
               return (
                 <div
@@ -199,7 +202,7 @@ function SettingsModal() {
         </div>
       </section>
       <button
-        className={`px-9 py-2.5 bg-[${initialColor}] rounded-full relative left-[13.5rem] top-[-20px] font-${initialFont} text-white font-bold `}
+        className={`px-9 py-2.5 bg-[${initialColor}] rounded-full relative left-[10rem] top-[25px] md:left-[13.5rem] md:top-[-20px] font-${initialFont} text-white font-bold `}
         onClick={() => handleSubmit()}
       >
         Apply
